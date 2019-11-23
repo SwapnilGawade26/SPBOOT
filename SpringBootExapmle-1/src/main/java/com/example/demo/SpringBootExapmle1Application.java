d11217b024b1b0f1ddd63bb65e89b645966ef7d7
SwapnilGawade26 <swapg26@gmail.com>
2019-11-23 18:14:16 +0530

    first commit

---
+++ SpringBootExapmle-1/src/main/java/com/example/demo/SpringBootExapmle1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExapmle1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExapmle1Application.class, args);
+	}
+
+}
